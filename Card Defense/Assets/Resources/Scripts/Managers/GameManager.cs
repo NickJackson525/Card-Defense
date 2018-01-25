ddbe04e0ba2b50d210941c0d39570be34a76b728
NickJackson525 <njacksonco>
2018-01-24 23:35:54 -0700

    card art and tile script

---
+++ Card Defense/Assets/Resources/Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Singleton
+
+    // create variable for storing singleton that any script can access
+    private static GameManager instance;
+
+    // create GameManager
+    private GameManager()
+    {
+
+    }
+
+    // Property for Singleton
+    public static GameManager Instance
+    {
+        get
+        {
+            // If the singleton does not exist
+            if (instance == null)
+            {
+                // create and return it
+                instance = new GameManager();
+            }
+
+            // otherwise, just return it
+            return instance;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+}
